31a4387e8f2cf3d7dfeeacf3131d42db76248db7
pingnattawoot <nattawoot.s@taskworld.com>
2019-02-06 03:19:53 +0700

    setup essential dependency for project

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,32 @@
   </div>
 </template>
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
+@font-face {
+  font-family: 'Thai Sans Neue';
+  src: url('assets/fonts/ThaiSansNeue-Regular.ttf') format('truetype');
+  font-weight: normal;
+  font-style: normal;
+}
 
-#nav
-  padding 30px
-  a
-    font-weight bold
-    color #2c3e50
-    &.router-link-exact-active
-      color #42b983
+#app {
+  // font-family: 'Sukhumvit Set', 'Thai Sans Neue', sans-serif;
+  font-family: 'Thai Sans Neue', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld msg="ทดสอบภาษาไทย" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
 
-@Component({
+export default {
   components: {
-    HelloWorld
-  }
-})
-export default class Home extends Vue {}
+    HelloWorld,
+  },
+}
 </script>
