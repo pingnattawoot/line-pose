51c138127bccf8f39da94ca89e37b5c6c0225923
pingnattawoot <nattawoot.s@taskworld.com>
2019-02-10 03:38:04 +0700

    start adding test

---
+++ tests/unit/utils/getThaiBahtText.spec.ts
@@ -0,0 +1,19 @@
+import { getThaiBahtText } from '@/utils'
+
+describe('[Util] Get Thai Baht Text :', () => {
+  it('should contain Thai baht sign (฿)', () => {
+    const value = 123
+    const result = getThaiBahtText(value)
+    expect(result).toMatch(/฿/)
+  })
+  it('should render comma if value >= 1000', () => {
+    const value = 1000
+    const result = getThaiBahtText(value)
+    expect(result).toMatch(/\,/)
+  })
+  it('should not render comma if value < 1000', () => {
+    const value = 999
+    const result = getThaiBahtText(value)
+    expect(result).not.toMatch(/\,/)
+  })
+})
